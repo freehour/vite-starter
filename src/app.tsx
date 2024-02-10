ac7838d6edc59b47eefe33f1e06aa137d5a102c6
freehour <ka1@posteo.de>
2024-02-10 13:31:51 +0100

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+    return <div>Hello World</div>;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './app.tsx';
+
+import './index.css';
+
+const root = document.getElementById('root');
+if (root) {
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
+} else {
+    throw new Error('Root element not found');
+}
